720e8e0442b008743d66c47c78c088c23d096f81
Joe Lafiosca <joe@lafiosca.com>
2020-07-30 22:53:13 -0400

    noodling with client module approach

---
+++ packages/rosebus-client/src/TextLog.tsx
@@ -0,0 +1,100 @@
+import React, {
+	FunctionComponent,
+	useState,
+	useEffect,
+} from 'react';
+import { Observable } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
+
+const actionTypes = {
+	logMessage: 'TextLog.logMessage',
+	clearMessages: 'TextLog.clearMessages',
+};
+
+interface Action {
+	type: string;
+	fromModuleId: string;
+	targetModuleId?: string;
+	targetScreenId?: string;
+	payload: any;
+}
+
+interface Props {
+	dispatch: (action: Action) => void;
+	storage: {
+		fetch: <T>(key: string) => Promise<T | undefined>;
+		store: <T>(key: string, value: T) => Promise<void>;
+		remove: (key: string) => Promise<void>;
+	};
+	action$: Observable<Action>;
+}
+
+const TextLog: FunctionComponent<Props> = ({
+	storage,
+	action$,
+}) => {
+	const [logMessages, setLogMessages] = useState<string[]>([]);
+
+	useEffect(
+		() => {
+			(async () => {
+				const storedLogMessages = await storage.fetch<string[]>('logMessages');
+				if (storedLogMessages) {
+					setLogMessages((oldLogMessages) => {
+						if (oldLogMessages.length > 0) {
+							const newLogMessages = [
+								...storedLogMessages,
+								...oldLogMessages,
+							].slice(-10);
+							storage.store('logMessages', newLogMessages);
+							return newLogMessages;
+						}
+						return storedLogMessages;
+					});
+				}
+			})();
+		},
+		[storage],
+	);
+
+	useEffect(
+		() => {
+			const logMessageAction$ = action$.pipe(
+				filter((action) => action.type === actionTypes.logMessage),
+				tap(({ payload }) => {
+					if (typeof payload !== 'string') {
+						// complain?
+					} else {
+						setLogMessages((oldLogMessages) => {
+							const newLogMessages = [...oldLogMessages.slice(-9), payload];
+							storage.store('logMessages', newLogMessages);
+							return newLogMessages;
+						});
+					}
+				}),
+			).subscribe();
+			const clearMessagesAction$ = action$.pipe(
+				filter((action) => action.type === actionTypes.clearMessages),
+				tap(() => {
+					setLogMessages([]);
+					storage.store('logMessages', []);
+				}),
+			).subscribe();
+			return () => {
+				logMessageAction$.unsubscribe();
+				clearMessagesAction$.unsubscribe();
+			};
+		},
+		[action$, storage],
+	);
+
+	return (
+		<div>
+			{logMessages.map((message) => (
+				<p>{message}</p>
+			))}
+		</div>
+	);
+};
+
+export default TextLog;
